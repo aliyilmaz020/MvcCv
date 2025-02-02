f9be77f2136d9dfc7425bd7780425ce9d1d5a522
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2025-02-02 22:00:42 +0300

    Experience CRUD

---
+++ MvcCv/Controllers/ExperienceController.cs
@@ -0,0 +1,53 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        // GET: Experience
+        ExperienceRepository repo = new ExperienceRepository();
+        public ActionResult Index()
+        {
+            var values = repo.List();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult AddExperience()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddExperience(Experience e)
+        {
+            repo.TAdd(e);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Remove(Experience e)
+        {   
+            var value = repo.TGet(e.ID);
+            repo.TRemove(value);
+            return RedirectToAction("Index");
+        }
+        public ActionResult GetExperience(int id)
+        {
+            var value = repo.TGet(id);
+            return View(value);
+        }
+        public ActionResult Edit(Experience e)
+        {
+            var updateValue = repo.TGet(e.ID);
+            updateValue.Title = e.Title;
+            updateValue.Description = e.Description;
+            updateValue.SubTitle = e.SubTitle;
+            updateValue.Date = e.Date;
+            repo.TUpdate(updateValue);
+            return RedirectToAction("Index");
+        }
+    }
+}
