2bc8e292427b51ac3a4c7bdba70cf9707af3589f
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2025-02-02 18:58:04 +0300

    CV

--- MvcCv/Controllers/DefaultController.cs
+++ MvcCv/Controllers/DefaultController.cs
@@ -1,13 +1,56 @@
-﻿using System.Web.Mvc;
+﻿using MvcCv.Models.Entity;
+using System;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace MvcCv.Controllers
 {
     public class DefaultController : Controller
     {
+        DbCvEntities db = new DbCvEntities();
         // GET: Default
         public ActionResult Index()
         {
-            return View();
+            var value = db.Abouts.ToList();
+            return View(value);
+        }
+        public PartialViewResult Experience()
+        {
+            var values = db.Experiences.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult Education()
+        {
+            var values = db.Educations.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult Talent()
+        {
+            var values = db.Talents.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult Hobby()
+        {
+            var values = db.Hobbies.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult Certificates()
+        {
+            var values = db.Awards.ToList();
+            return PartialView(values);
+        }
+        [HttpGet]
+        public PartialViewResult Contact()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult Contact(Contact c)
+        {
+            c.Date = DateTime.Now;
+            db.Contacts.Add(c);
+            db.SaveChanges();
+            return PartialView();
         }
     }
 }
