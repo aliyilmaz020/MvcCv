f0cb1e51a6a1c80f0c09a1eb6bb21cc40b9b453b
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2025-02-03 16:40:36 +0300

    Education List and add education

---
+++ MvcCv/Controllers/EducationController.cs
@@ -0,0 +1,32 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class EducationController : Controller
+    {
+        // GET: Education
+        GenericRepository<Education> repo = new GenericRepository<Education>();
+        public ActionResult Index()
+        {
+            var values = repo.List();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult AddEducation()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddEducation(Education e)
+        {
+            repo.TAdd(e);
+            return RedirectToAction("Index");
+        }
+    }
+}
