878e2077c338f739f2a6fb03f808bb8b6f4f20f6
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2025-02-04 00:15:58 +0300

    Delete Certificate

--- MvcCv/Controllers/CertificateController.cs
+++ MvcCv/Controllers/CertificateController.cs
@@ -3,15 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcCv.Models.Entity;
+using MvcCv.Repositories;
 
 namespace MvcCv.Controllers
 {
     public class CertificateController : Controller
     {
         // GET: Certificate
+        GenericRepository<Award> repo = new GenericRepository<Award>();
         public ActionResult Index()
         {
+            var values = repo.List();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var value = repo.TGet(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult Edit(Award a)
+        {
+            var award = repo.TGet(a.ID);
+            award.Description = a.Description;
+            award.Date = a.Date;
+            repo.TUpdate(award);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult AddCertificate()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult AddCertificate(Award a)
+        {
+            repo.TAdd(a);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Remove(int id)
+        {
+            var value = repo.TGet(id);
+            repo.TRemove(value);
+            return RedirectToAction("Index");
         }
     }
 }
