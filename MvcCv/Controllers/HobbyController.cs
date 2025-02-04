5960ab948b17be763d1f20835e51615fd6e028f7
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2025-02-04 21:41:25 +0300

    Hobby view controller

---
+++ MvcCv/Controllers/HobbyController.cs
@@ -0,0 +1,27 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class HobbyController : Controller
+    {
+        // GET: Hobby
+        GenericRepository<Hobby> repo = new GenericRepository<Hobby>();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var values = repo.List();
+            return View(values);
+        }
+        [HttpPost]
+        public ActionResult Index(Hobby h)
+        {
+            var value = repo.TGet(h.ID);
+            value.Description = h.Description;
+            value.Description2 = h.Description2;
+            repo.TUpdate(value);
+            return RedirectToAction("Index");
+        }
+    }
+}
