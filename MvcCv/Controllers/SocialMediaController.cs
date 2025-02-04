f40ced1a10296856ed88b80a6849e814b5a2afa3
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2025-02-04 23:35:18 +0300

    Social media pop up add and update

--- MvcCv/Controllers/SocialMediaController.cs
+++ MvcCv/Controllers/SocialMediaController.cs
@@ -17,5 +17,32 @@ namespace MvcCv.Controllers
             var values = repo.List();
             return View(values);
         }
+        [HttpGet]
+        public ActionResult AddSocialMedia()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddSocialMedia(SocialMedia s)
+        {
+            repo.TAdd(s);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var media = repo.TGet(id);
+            return View(media);
+        }
+        [HttpPost]
+        public ActionResult Edit(SocialMedia s)
+        {
+            var media = repo.TGet(s.ID);
+            media.Name = s.Name;
+            media.Link = s.Link;
+            media.Icon = s.Icon;
+            repo.TUpdate(media);
+            return RedirectToAction("Index");
+        }
     }
 }
