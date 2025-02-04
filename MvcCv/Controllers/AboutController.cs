2f982f9c29fe2b85ceb20bac9adc54ebca6a7e46
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2025-02-04 21:55:50 +0300

    About

--- MvcCv/Controllers/AboutController.cs
+++ MvcCv/Controllers/AboutController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,26 @@ namespace MvcCv.Controllers
     public class AboutController : Controller
     {
         // GET: About
+        AboutRepository repo = new AboutRepository();
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            var values = repo.List();
+            return View(values);
+        }
+        [HttpPost]
+        public ActionResult Index(About a)
+        {
+            var value = repo.TGet(a.ID);
+            value.Name = a.Name;
+            value.Surname = a.Surname;
+            value.Description = a.Description;
+            value.Address = a.Address;
+            value.Phone = a.Phone;
+            value.Mail = a.Mail;
+            value.Image = a.Image;
+            repo.TUpdate(value);
+            return RedirectToAction("Index");
         }
     }
 }
