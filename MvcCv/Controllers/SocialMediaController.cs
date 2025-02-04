116d5ad49e5e2815a2dccc10bd93a328d7f600b0
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2025-02-04 22:43:31 +0300

    Social Media Index

---
+++ MvcCv/Controllers/SocialMediaController.cs
@@ -0,0 +1,21 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        // GET: SocialMedia
+        GenericRepository<SocialMedia> repo = new GenericRepository<SocialMedia>();
+        public ActionResult Index()
+        {
+            var values = repo.List();
+            return View(values);
+        }
+    }
+}
